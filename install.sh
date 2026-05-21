8da4b1e2b0d165be3fd67bb7a87efe36ccdda6d2
dec32 <56337978+dec32@users.noreply.github.com>
2026-05-21 21:51:21 +0800

    enhance: detect system proxy when installing

--- install.sh
+++ install.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+# Detect Proxy
+os_type="$(uname -s)"
+proxy=""
+
+if [[ "$os_type" == MINGW* || "$os_type" == MSYS* || "$os_type" == CYGWIN* ]]; then
+    proxy=$(reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" | grep -oE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+")
+elif [[ "$os_type" == "Darwin" ]]; then
+    port=$(networksetup -getwebproxy "Wi-Fi" 2>/dev/null | grep Port | awk '{print $2}')
+    server=$(networksetup -getwebproxy "Wi-Fi" 2>/dev/null | grep Server | awk '{print $2}')
+    
+    if [[ "$port" == "0" || -z "$server" ]]; then
+        port=$(networksetup -getwebproxy "Ethernet" 2>/dev/null | grep Port | awk '{print $2}')
+        server=$(networksetup -getwebproxy "Ethernet" 2>/dev/null | grep Server | awk '{print $2}')
+    fi
+
+    if [[ "$port" != "0" && -n "$server" ]]; then
+        proxy="$server:$port"
+    fi
+fi
+
+## Apply proxy
+if [[ -n "$proxy" ]]; then
+    export http_proxy="http://$proxy"
+    export https_proxy="http://$proxy"
+    echo "Proxy detected: $proxy"
+else
+    echo "No proxy detected, using direct connection."
+fi
+
 # Target download URL
 URL="https://github.com/rimeinn/rime-moran/releases/latest/download/Trad-FullPack.zip"
 ZIP_NAME="Trad-FullPack.zip"
